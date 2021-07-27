f4e4c9f90232b0bcd182a300faf491ca3f409151
Yaqza <75371978+yaqza29@users.noreply.github.com>
2021-07-27 20:12:07 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 300,130)
+    fill("lightblue")
+    text("Hint: Stores all of the info !!", 300,150)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 100,200)
+    fill("lightblue")
+    text("Hint: To perform a task !!", 100,220)
+    
+
+}
